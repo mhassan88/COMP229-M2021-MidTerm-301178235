298d2c5b20f7adce59e3729d591a94a16f69f2eb
Muhammad Hassan <hassaniq@live.com>
2021-06-24 20:41:30 -0400

    did remote db configurations

--- Server/Config/app.js
+++ Server/Config/app.js
@@ -29,7 +29,10 @@ const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const morgan_1 = __importDefault(require("morgan"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const DBConfig = __importStar(require("./db"));
-mongoose_1.default.connect(process.env.URI || DBConfig.LocalURI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose_1.default.connect(process.env.URI || DBConfig.RemoteURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 const db = mongoose_1.default.connection;
 db.on("error", function () {
     console.error("connection error");
@@ -41,23 +44,23 @@ const index_1 = __importDefault(require("../Routes/index"));
 const books_1 = __importDefault(require("../Routes/books"));
 const app = express_1.default();
 exports.default = app;
-app.set('views', path_1.default.join(__dirname, '../Views'));
-app.set('view engine', 'ejs');
-app.use(morgan_1.default('dev'));
+app.set("views", path_1.default.join(__dirname, "../Views"));
+app.set("view engine", "ejs");
+app.use(morgan_1.default("dev"));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use(cookie_parser_1.default());
-app.use(express_1.default.static(path_1.default.join(__dirname, '../../Client')));
-app.use(express_1.default.static(path_1.default.join(__dirname, '../../node_modules')));
-app.use('/', index_1.default);
-app.use('/books', books_1.default);
+app.use(express_1.default.static(path_1.default.join(__dirname, "../../Client")));
+app.use(express_1.default.static(path_1.default.join(__dirname, "../../node_modules")));
+app.use("/", index_1.default);
+app.use("/books", books_1.default);
 app.use(function (req, res, next) {
     next(http_errors_1.default(404));
 });
 app.use(function (err, req, res, next) {
     res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.error = req.app.get("env") === "development" ? err : {};
     res.status(err.status || 500);
-    res.render('error');
+    res.render("error");
 });
 //# sourceMappingURL=app.js.map
--- Server/Config/db.js
+++ Server/Config/db.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://localhost/books229";
-exports.RemoteURI = "your remote host information goes here";
-exports.HostName = "LocalHost";
+exports.RemoteURI = "mongodb+srv://hassan:mymongo@clusterportfolio.qfnax.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+exports.HostName = "RemoteHost";
 exports.Secret = "someSecret";
 //# sourceMappingURL=db.js.map
