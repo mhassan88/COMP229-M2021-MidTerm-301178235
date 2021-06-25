f3bc6c3212705ca8380943f2a54f92585e264034
Muhammad Hassan <hassaniq@live.com>
2021-06-24 20:39:32 -0400

    did remote db configurations

--- .history/Server/Config/db_20210624203529.ts
+++ .history/Server/Config/db_20210624203529.ts
@@ -1,5 +1,5 @@
 export const LocalURI = "mongodb://localhost/books229";
 export const RemoteURI =
   "mongodb+srv://hassan:mymongo@clusterportfolio.qfnax.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-export const HostName = "RemoteHost";
+export const HostName = "LocalHost";
 export const Secret = "someSecret";
